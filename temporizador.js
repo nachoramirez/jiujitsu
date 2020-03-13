50662f9c2f36dabbac55deddc30fd377872c4f03
Nacho <nachor7803@gmail.com>
2020-03-12 23:39:03 -0300

    inicio del proyecto

---
+++ temporizador.js
@@ -0,0 +1,25 @@
+/*
+function StartTime() {
+    seconds = 0
+    segundos = document.getElementById('segundos')
+    minutos = document.getElementById('minutos')
+
+    cronometro = setInterval(() => {
+        seconds++
+
+        secs = seconds
+        mins = 0
+
+        while (secs >= 60){
+            mins++
+            secs -= 60
+        }
+
+        minutos.innerHTML = (mins<10)? "0" + mins : mins;
+        segundos.innerHTML = (secs<10)? "0" + secs : secs;
+
+    }, 1000)
+}
+
+StartTime()
+*/
