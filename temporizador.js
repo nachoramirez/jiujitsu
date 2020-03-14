d3d8650e118f51770c642ef075365bcaec61fced
Nacho <nachor7803@gmail.com>
2020-03-14 20:18:30 -0300

    agregando el pause en el cronometro

--- temporizador.js
+++ temporizador.js
@@ -1,24 +1,39 @@
-
-function StartTime() {
-    seconds = 0
+    
+    time = document.getElementsByClassName('time')
     segundos = document.getElementById('segundos')
     minutos = document.getElementById('minutos')
 
-    cronometro = setInterval(() => {
-        seconds++
+    verificador = false
+    seconds = 0
 
-        secs = seconds
-        mins = 0
+    function togglePause(){
+        if (verificador){
+            verificador = false
+        }else{
+            verificador = true
+        }
+    }
 
-        while (secs >= 60){
-            mins++
-            secs -= 60
+
+    setInterval(() => {
+        if(verificador){
+            seconds++
+
+            secs = seconds
+            mins = 0
+
+            while (secs >= 60){
+                mins++
+                secs -= 60
+            }
+
+            minutos.innerHTML = (mins<10)? "0" + mins : mins;
+            segundos.innerHTML = (secs<10)? "0" + secs : secs;
         }
 
-        minutos.innerHTML = (mins<10)? "0" + mins : mins;
-        segundos.innerHTML = (secs<10)? "0" + secs : secs;
-
     }, 1000)
-}
 
-StartTime()
+
+
+
+
