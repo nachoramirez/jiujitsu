45ee561dfd65c67abe404c5b2089a088755692ea
Nacho <nachor7803@gmail.com>
2020-03-19 23:08:27 -0300

    agregando final del cronometro y todos los botones

--- puntos1.js
+++ puntos1.js
@@ -7,34 +7,62 @@ var puntos_uno = 0
 var ventajas_uno = 0
 var penalizaciones_uno = 0
 
+var Acciones1 =[]
 
-function puntoUno(){
-    puntos_uno++
+
+function SumarDos1(){
+    puntos_uno += 2
     puntos1.innerHTML = puntos_uno
+    accion = "Se sumaron dos puntos"
+    Acciones1.push(accion)
 }
 
-function ventajasUno(){
+function SumarTres1(){
+    puntos_uno += 3
+    puntos1.innerHTML = puntos_uno
+    accion = "Se sumaron tres puntos"
+    Acciones1.push(accion)
+}
+
+function SumarCuatro1(){
+    puntos_uno += 4
+    puntos1.innerHTML = puntos_uno
+    accion = "Se sumaron cuatro puntos"
+    Acciones1.push(accion)
+}
+
+function ventajasUno() {
     ventajas_uno++
     ven1.innerHTML = ventajas_uno
+    accion = "Se sumo una ventaja"
+    Acciones1.push(accion)
 }
 
-function penalizacionesUno(){
+function penalizacionesUno() {
     penalizaciones_uno--
     pen1.innerHTML = penalizaciones_uno
+    accion = "Se penalizo"
+    Acciones1.push(accion)
 }
 
-function restarPuntosUno(){
+function restarPuntosUno() {
     puntos_uno--
     puntos1.innerHTML = puntos_uno
+    accion = "Se resto un punto"
+    Acciones1.push(accion)
 }
 
-function restarVenUno(){
+function restarVenUno() {
     ventajas_uno--
     ven1.innerHTML = ventajas_uno
+    accion = "Se resto una ventaja"
+    Acciones1.push(accion)
 }
 
 
-function restarPenUno(){
+function restarPenUno() {
     penalizaciones_uno++
     pen1.innerHTML = penalizaciones_uno
+    accion = "Se resto la penalizacion"
+    Acciones1.push(accion)
 }
--- temporizador.js
+++ temporizador.js
@@ -1,38 +1,49 @@
-    
-    time = document.getElementsByClassName('time')
-    segundos = document.getElementById('segundos')
-    minutos = document.getElementById('minutos')
 
-    verificador = false
-    seconds = 0
+time = document.getElementsByClassName('time')
+segundos = document.getElementById('segundos')
+minutos = document.getElementById('minutos')
 
-    function togglePause(){
-        if (verificador){
-            verificador = false
-        }else{
-            verificador = true
+var user_time = prompt("¿Cuanto tiempo dura?", "5");
+
+if (user_time == null || user_time == "") {
+    fight_time = 5
+} else {
+    fight_time = user_time
+}
+
+
+verificador = false
+seconds = 0
+
+function togglePause() {
+    if (verificador) {
+        verificador = false
+    } else {
+        verificador = true
+    }
+}
+
+
+setInterval(() => {
+    if (verificador) {
+        seconds++
+
+        secs = seconds
+        var mins = 0
+
+        while (secs >= 60) {
+            mins++
+            secs -= 60
         }
+
+        minutos.innerHTML = (mins < 10) ? "0" + mins : mins;
+        segundos.innerHTML = (secs < 10) ? "0" + secs : secs;
     }
 
-
-    setInterval(() => {
-        if(verificador){
-            seconds++
-
-            secs = seconds
-            mins = 0
-
-            while (secs >= 60){
-                mins++
-                secs -= 60
-            }
-
-            minutos.innerHTML = (mins<10)? "0" + mins : mins;
-            segundos.innerHTML = (secs<10)? "0" + secs : secs;
-        }
-    }, 1000)
-
-
-
+    if(fight_time <= mins){
+        verificador = false
+    }
+    
+}, 1000)
 
 
