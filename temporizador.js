d8b582bba6e488ba8c5dd6631f4e4c51d9769c86
Nacho <nachor7803@gmail.com>
2020-03-25 01:30:11 -0300

    agregando fondo, cambios de letra, el mostrar resultados y final con sonidos

--- temporizador.js
+++ temporizador.js
@@ -2,24 +2,30 @@
 time = document.getElementsByClassName('time')
 segundos = document.getElementById('segundos')
 minutos = document.getElementById('minutos')
+audio = document.getElementById("audio");
 
-var user_time = prompt("¿Cuanto tiempo dura?", "5");
-
-if (user_time == null || user_time == "") {
-    fight_time = 5
-} else {
-    fight_time = user_time
-}
-
-
+started = false
 verificador = false
 seconds = 0
 
 function togglePause() {
-    if (verificador) {
-        verificador = false
-    } else {
+    if (!started) {
+        var user_time = prompt("¿Tiempo de lucha?", "5",type="number");
+
+        if (user_time == null || user_time == "") {
+            fight_time = 5
+        } else {
+            fight_time = user_time
+        }
+        started = true
         verificador = true
+
+    } else {
+        if (verificador) {
+            verificador = false
+        } else {
+            verificador = true
+        }
     }
 }
 
@@ -31,19 +37,33 @@ setInterval(() => {
         secs = seconds
         var mins = 0
 
-        while (secs >= 60) {
+        while (secs >= 10) {
             mins++
-            secs -= 60
+            secs -= 10
         }
 
         minutos.innerHTML = (mins < 10) ? "0" + mins : mins;
         segundos.innerHTML = (secs < 10) ? "0" + secs : secs;
+
+        if (fight_time <= mins) {
+            verificador = false
+            audio.play()
+            document.getElementById("resultados").style.visibility = "visible";
+        }
     }
 
-    if(fight_time <= mins){
-        verificador = false
-    }
     
+
 }, 1000)
 
 
+function MostrarResultados(){
+    console.log('Al luchador blanco:')
+    for(i=0;i<Acciones1.length;i++){
+        console.log('    ' + Acciones1[i])
+    }
+    console.log('Al luchador amarillo:')
+    for(i=0;i<Acciones2.length;i++){
+        console.log('    ' + Acciones2[i])
+    }
+}
